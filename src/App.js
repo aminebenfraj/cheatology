1164f10610a0060500813e3b180467686cd2237e
amineBenFraj <mohamedamine.benfredj@polytechnicien.tn>
2024-09-27 11:42:23 +0100

    full project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import Introduction from "./components/Introduction";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+        <Header />
+        <Introduction />
+        <Services />
+        <Contact />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
